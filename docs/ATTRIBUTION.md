ce9c16cacecdc432513b40132e6cfc6c75bcd59a
Roy R <tchalvakspam@gmail.com>
2019-01-18 19:32:08 -0500

    Update with new site favicon.

---
+++ docs/ATTRIBUTION.md
@@ -0,0 +1,3 @@
+Design Creative Commons Attribution 3.0 License by HTML5 UP:
+
+https://html5up.net/license
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "Ra AI communication",
+    author: "Roy Ronalds",
+    description: "A site for showcasing AI jamstack integrations."
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -12,10 +12,10 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#d422ff',
+        theme_color: '#d422ff',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/ra-icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
