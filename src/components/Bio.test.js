dd2b7ae9e7fd6fd2442a5f16efc36d8927024398
Roy R <tchalvakspam@gmail.com>
2019-01-19 21:57:53 -0500

    Fix person assignment.

--- src/components/Bio.test.js
+++ src/components/Bio.test.js
@@ -4,6 +4,10 @@ import Bio from "./Bio"
 
 describe("Bio", () => {
   it("renders correctly", () => {
+    const person = {
+      name: "Test name",
+      phone: '15556667777'
+    };
     const tree = renderer.create(<Bio person={{}} />).toJSON()
     expect(tree).toMatchSnapshot()
   })
